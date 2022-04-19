9504b6f01f9274bad9da2ad039249e30326416fa
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2022-04-18 18:59:12 -0700

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,49 +1,49 @@
 job('Java Maven App DSL 3') {
-    description('Java Maven App con DSL para el curso de Jenkins')
-    scm {
-        git('https://github.com/Ramonst18/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('Ramonst18')
-            node / gitConfigEmail('Ramonst18@hotmail.com')
-        }
+  description('Java Maven App con DSL para el curso de Jenkins')
+  scm {
+    git('https://github.com/Ramonst18/simple-java-maven-app.git', 'master') { node ->
+      node / gitConfigName('Ramonst18')
+      node / gitConfigEmail('Ramonst18@hotmail.com')
     }
-    triggers {
-    	githubPush()
-    }    
-    steps {
-        maven {
-          mavenInstallation('mavenjenkins')
-          goals('-B -DskipTests clean package')
-        }
-        maven {
-          mavenInstallation('mavenjenkins')
-          goals('test')
-        }
-        shell('''
-          echo "Entrega: Desplegando la aplicación" 
-          java -jar "/var/jenkins_home/workspace/Java Maven App DSL 3/target/my-app-1.0-SNAPSHOT.jar"
-        ''')  
+  }
+  triggers {
+    githubPush()
+  }    
+  steps {
+    maven {
+      mavenInstallation('mavenjenkins')
+      goals('-B -DskipTests clean package')
     }
-    publishers {
-        archiveArtifacts('target/*.jar')
-        archiveJunit('target/surefire-reports/*.xml')
-	      slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-       }
+    maven {
+      mavenInstallation('mavenjenkins')
+      goals('test')
     }
+    shell('''
+    echo "Entrega: Desplegando la aplicación" 
+    java -jar "/var/jenkins_home/workspace/Java Maven App DSL 3/target/my-app-1.0-SNAPSHOT.jar"
+    ''')  
+  }
+  publishers {
+    archiveArtifacts('target/*.jar')
+    archiveJunit('target/surefire-reports/*.xml')
+	  slackNotifier {
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(true)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
 }
 
 job('Job test Hola Mundo') {
